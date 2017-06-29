c44fb9f4e5ccbd1cf02d728eca78eb79ce923df4
Pierre Rineau <pierre.rineau@makina-corpus.com>
2017-06-29 17:51:01 +0200

    better php-calista usage

--- calista.container.php
+++ calista.container.php
@@ -3,13 +3,9 @@
 namespace Drupal\Module\calista;
 
 use Drupal\Core\DependencyInjection\ServiceProviderInterface;
-use MakinaCorpus\Calista\DependencyInjection\Compiler\ActionProviderRegisterPass;
-use MakinaCorpus\Calista\DependencyInjection\Compiler\DowngradeCompatibilityPass;
-use MakinaCorpus\Calista\DependencyInjection\Compiler\PageDefinitionRegisterPass;
-use MakinaCorpus\Calista\DependencyInjection\Compiler\RegisterTemplatePass;
+use MakinaCorpus\Calista\CalistaBundle;
 use MakinaCorpus\Drupal\Calista\DependencyInjection\Compiler\ActionProcessorRegisterPass;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
@@ -23,14 +19,20 @@ class ServiceProvider implements ServiceProviderInterface
      */
     public function register(ContainerBuilder $container)
     {
+        // @todo check if bundle is present
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/Resources/config'));
-        $loader->load('core.yml');
         $loader->load('drupal.yml');
 
-        $container->addCompilerPass(new DowngradeCompatibilityPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 50 /* Make it run before twig's one */);
-        $container->addCompilerPass(new ActionProviderRegisterPass());
         $container->addCompilerPass(new ActionProcessorRegisterPass());
-        $container->addCompilerPass(new PageDefinitionRegisterPass(), PassConfig::TYPE_BEFORE_REMOVING);
-        $container->addCompilerPass(new RegisterTemplatePass());
+    }
+
+    /**
+     * {@inhertidoc}
+     */
+    public function registerBundles()
+    {
+        return [
+            new CalistaBundle(),
+        ];
     }
 }
